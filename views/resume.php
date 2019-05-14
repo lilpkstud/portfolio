703784885170d93c34377c7a32801749e13d32ae
lilpkstud <lilpkstud@hotmail.com>
2019-05-14 16:00:53 -0700

    Added CSS and IMG

---
+++ views/resume.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<?php //include_once("../analyticstracking.php") ?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="author" content="Paul Kwon">
+    <title>Paul Kwon Resume</title>
+    <script type="text/javascript">
+        function load() 
+        {
+            window.location.href = "../img/PaulKwonResume2018.pdf";
+        }
+    </script>
+    <!-- Global Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-91000241-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-91000241-1');
+    </script>
+</head>
+<body onload="load()">
+<?php include_once("../analyticstracking.php") ?>
+</body>
+</html>
