b7a91d63945cd10c84374397d4ef49ae6037adab
lilpkstud <lilpkstud@hotmail.com>
2019-07-26 15:58:21 -0700

    Adding Meta Tags

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title> Paul Kwon | Home</title>
+    <meta name="description" content="Paul Kwon - Project Manager and Freelance Web Developer Portfolio">
+    <meta name="keywords" content="Paul Kwon, Kwon, paulskwon, project manager, portfolio">
+    <meta name="author" content="Paul Kwon">
     
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
