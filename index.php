6bb570f7804ecbfbddea7ea2666a6859d70bf099
lilpkstud <lilpkstud@hotmail.com>
2019-08-16 19:52:20 -0700

    Aug 16 Updates

--- index.php
+++ index.php
@@ -62,6 +62,26 @@
                         <h4 class='aboutMe'> <strong> About Me: </strong> <i class="em em-kr"></i><i class="em em-us"></i>,  <i class="em em-church"></i>,  <i class="em em-coffee"></i>,  <i class="em em-beer"></i>,  <i class="em em-basketball"></i>,  <i class="em em-man-lifting-weights"></i>,  <i class="em em-snow_capped_mountain"></i>,  <i class="em em-golf"></i>
                         </h4>
                         <p>Clarifications about the emojis can be found  <a href="#about"> here</a>. </p>
+                        <h4> 
+                            <a href="mailto:paul.s.kwon24@gmail.com" style="text-decoration: none;">
+                                <span style="font-size: 1.5em;">
+                                    <i class="fas fa-envelope"></i>
+                                </span>
+                                
+                            </a>
+                            |
+                            <a href="https://www.linkedin.com/in/pskwon" target="_blank" style="text-decoration:none;">
+                                <span style="font-size: 1.5em;">
+                                    <i class="fab fa-linkedin-in"></i>
+                                </span>
+                            </a> 
+                            |
+                            <a href="https://github.com/lilpkstud" target="_blank" style="text-decoration:none;">
+                                <span style="font-size: 1.5em;">
+                                    <i class="fab fa-github"></i>
+                                </span>
+                            </a>
+                        </h4>
                     </div>
                 </div>
             </section>
@@ -70,9 +90,9 @@
                 <h3>Projects</h3>
                 <hr style='border-width: 3px'>
                 <div class='row container-fluid '>
-                    <div class='col-lg-4 col-sm-6 project'>
-                        <a href="/views/projects/homebuilding.php">
-                            <img class='project_img' src="/img/projects/home.jpg" alt="">
+                    <div class='col-lg-4 col-md-6 project'>
+                        <a href="/views/homebuilding.php" target="_blank">
+                            <img class='project_img img-fluid' src="/img/projects/home.jpg" alt="">
                             <div class='overlay'>
                                 <h3 class='title'>Homebuilding Case</h3>
                                 <p class='projectType'><strong>(Project Management)</strong></p>
@@ -80,82 +100,87 @@
                             </div>
                         </a>
                     </div>
-                    <div class='col-lg-4 col-sm-6 project'>
-                        <a href="http://www.sharonharrah.com">
-                        <!-- Working File: <a href="/views/lunchbox/index.php" target='_blank'> -->
-                            <img class='project_img' src="/img/projects/sharon.png" alt="">
+                    <div class='col-lg-4 col-md-6 project'>
+                        <a href="http://www.sharonharrah.com" target="_blank" style="text-decoration:none;">
+                            <img class='project_img img-fluid' src="/img/projects/sharon.png" alt="">
                             <div class='overlay'>
                                 <h3 class='title'>Sharon Harrah</h3>
-                                <p class='projectType'><strong>(Freelance)</strong></p>
-                                <p class='projectText'>Social Media & Content Marketing specialist's portfolio</p>
-                                <p class='underDevelopment'> [Under Development]</p>
+                                <div class="projectType">
+                                    <p class="projectType"><strong>(Freelance)</strong></p>
+                                    <p style="color:whitesmoke; text-align:center;">Social Media & Content Marketing specialist's portfolio</p>
+                                    <a class="projectText btn btn-primary" href="https://github.com/lilpkstud/SharonPortfolio" role="button" style="margin: 0 10px;">View Code</a>
+                                    <a class="projectText btn btn-primary" href="http://www.sharonharrah.com" role="button">View Project</a>
+                                </div>
                             </div>
                         </a>
                     </div>
-                    <div class='col-lg-4 col-sm-6 project'>
-                        <a href="https://pauls-movies.herokuapp.com/" target='_blank'>
-                            <img class='project_img' src="/img/projects/movie.jpg" alt="">
+                    <div class='col-lg-4 col-md-6 project'>
+                        <a href="https://pauls-movies.herokuapp.com/" target='_blank' style="text-decoration:none;">
+                            <img class='project_img img-fluid' src="/img/projects/movie.jpg" alt="">
                             <div class='overlay'>
                                 <h3 class='title'>My Movies</h3>
-                                <p class='projectType'><strong>(Personal Project)</strong></p>
-                                <p class='projectText'>My personal korean movie/drama recommendations.</p>
+                                <div class="projectType">
+                                    <p class='projectType'><strong>(Personal Project)</strong></p>
+                                    <p style='color:whitesmoke; text-align:center;'>My personal korean movie/drama recommendations.</p>
+                                    <a class="projectText btn btn-primary" href="https://github.com/lilpkstud/PaulsMovie" role="button" style="margin: 0 10px;">View Code</a>
+                                    <a class="projectText btn btn-primary" href="https://pauls-movies.herokuapp.com/" role="button">View Project</a>
+                                </div>
                             </div>
                         </a>
                     </div>
-                    <div class='col-lg-4 col-sm-6 project'>
-                        <a href="/views/yellow/index.php" target='_blank'>
-                            <img class='project_img' src="/img/projects/yellow.jpg" alt="">
-                            <div class='overlay'>
-                                <h3 class='title'>Lots of Yellow</h3>
-                                <p class='projectType'><strong>(Freelance)</strong></p>
-                                <p class='projectText'>Photographer's portfolio to show her work and find future clients.</p>
-                            </div>
-                        </a>
-                    </div>
-                    <div class='col-lg-4 col-sm-6 project'>
-                        <a href="http://www.tcpcmuyg.com" target='_blank'>
-                            <img class='project_img' src="/img/projects/mu_logo.png" alt="">
+                    <div class='col-lg-4 col-md-6 project'>
+                        <a href="http://www.tcpcmuyg.com" target='_blank' style="text-decoration:none;">
+                            <img class='project_img img-fluid' src="/img/projects/mu_logo.png" alt="">
                             <div class='overlay'>
                                 <h3 class='title'>TCPCMUYG.COM</h3>
-                                <p class='projectType'><strong>(Freelance)</strong></p>
-                                <p class='projectText'>Responsive website built to inform users of latest events, pastor sermon notes, and information about the youth group.</p>
+                                <div class="projectType">
+                                    <p class='projectType'><strong>(Freelance)</strong></p>
+                                    <p class='projectText'>Responsive website built to inform users of latest events, pastor sermon notes, and information about the youth group.</p>
+                                    <a class="projectText btn btn-primary" href="https://github.com/lilpkstud/TCPCMU_Homepage" role="button" style="margin: 0 10px;">View Code</a>
+                                    <a class="projectText btn btn-primary" href="http://www.tcpcmuyg.com" role="button">View Project</a>
+                                </div>
                             </div>
                         </a>
                     </div>
-                    <div class='col-lg-4 col-sm-6 project'>
-                        <a href="https://github.com/lilpkstud/Travel-Buddy" target='_blank'>
-                            <img class='project_img' src="/img/projects/travel.jpg" alt="">
+                    <div class='col-lg-4 col-md-6 project'>
+                        <a href="https://github.com/lilpkstud/Travel-Buddy" target='_blank' style="text-decoration:none;">
+                            <img class='project_img img-fluid' src="/img/projects/travel.jpg" alt="">
                             <div class='overlay'>
                                 <h3 class='title'> Travel Buddy</h3>
-                                <p class='projectType'> <strong> (School: LAMP Stack Test where students had 4 hours to complete the project) </strong></p>
-                                <p class='projectText'>Fullstack website that allows users to create and share their own traveling plans to find others who are interested in joining. </p>
+                                <div class="projectType">
+                                    <p class='projectType'> <strong> (School: LAMP Stack Test where students had 4 hours to complete the project) </strong></p>
+                                    <p class='projectText'>Fullstack website that allows users to create and share their own traveling plans to find others who are interested in joining. </p>
+                                    <a class="projectText btn btn-primary" href="https://github.com/lilpkstud/Travel-Buddy" role="button" style="margin: 0 10px;">View Code</a>
+                                </div>
                             </div>
                         </a>
                     </div>
-                    <div class='col-lg-4 col-sm-6 project'>
-                        <a href="https://github.com/lilpkstud/Quotes" target='_blank'>
-                            <img class='project_img' src="/img/projects/quotes.jpg" alt="">
+                    <div class='col-lg-4 col-md-6 project'>
+                        <a href="https://github.com/lilpkstud/Quotes" target='_blank' style="text-decoration:none;">
+                            <img class='project_img img-fluid' src="/img/projects/quotes.jpg" alt="">
                             <div class='overlay'>
                                 <h3 class='title'> Quotes Generator</h3>
-                                <p class='projectType'><strong>(School: LAMP Stack Test where students had 4 hours to complete the project)</strong></p>
-                                <p class='projectText'>Fullstack website that allows users to create quotes while view and favorite other user's quotes.</p>
+                                <div class="projectType">
+                                    <p class='projectType'><strong>(School: LAMP Stack Test where students had 4 hours to complete the project)</strong></p>
+                                    <p class='projectText'>Fullstack website that allows users to create quotes while view and favorite other user's quotes.</p>
+                                    <a class="projectText btn btn-primary" href="https://github.com/lilpkstud/Quotes" role="button" style="margin: 0 10px;">View Code</a>
+                                </div>
                             </div>
                         </a>
                     </div>
                 </div>
             </section>
 
-            <section id='about'>
+            <section id='about' class="container-fluid">
                 <div id='aboutRow' class='row'>
-                    <div class='col-lg-6'>
-                        <div class='container'>
-                            <img id='PaulKwon'src="/img/mt_rainer.jpg" alt="">
-                        </div>
+                    <div class='col-md-12 col-lg-6'>
+                        <img class='img-fluid'src="/img/mt_rainer.jpg" alt="">
                     </div>
-                    <div class='col-lg-6'>
+                    <div id="bio" class='col-md-12 col-lg-6' style="border: solid">
                         <h1 style='text-align:center'>PAUL S. KWON</h1>
-                        <p class='bio'> My first "Hello, World" program was in Java when I was taking a computer science course during my sophomore year in college. I failed that course because of pure laziness, but I was fascinated on how I was able to create anything on my computer....just not in Java. I decided to take a three month coding program where I became proficient in the LAMP Stack while dabbling with the MEAN Stack.</p>
-                        <p class='bio'> I am currently attending the Univeristy of Washington to get my Project Management Certification in order to take the PMP Exam. I have previous experience as a Quality Assurance Test Engineer and a freelance web developer for local businesses. I am looking for Project Management roles while available to meet future clients to help build their website. </p>
+                        <p class='bioText'> My first "Hello, World" program was in Java when I was taking a computer science course during my sophomore year in college. I failed that course because of pure laziness, but I was fascinated on how I was able to create anything on my computer....just not in Java. I decided to take a three month coding program where I became proficient in the LAMP Stack while dabbling with the MEAN Stack.</p>
+                        <p class='bioText'> I also have experience as a project manager. I have recently attained my project management certification from the University of Washington in  2018. My capstone project is a perfect example where I successfully led my team to identify project requirements and specifications to create a defined scope and a Work Breakdown Structure (WBS) during project planning. I also developed estimates of activity resources and activity durations to create the project's schedule using Microsoft Project. </p>
+                        <p class="bioText">I am currently a freelance web developer that helps local businesses and organizations while also looking for project management positions to gain experience as a PM.</p>
                         <h3>ABOUT ME:</h3>
                         <ul>
                             <li class='aboutEmoji'> Korean-American <i class="em em-kr"></i><i class="em em-us"></i> </li>
@@ -167,12 +192,13 @@
                             <li class='aboutEmoji'> Hiking for the Gram  <i class="em em-snow_capped_mountain"></i> </li>
                             <li class='aboutEmoji'> Perfecting the Happy Gilmore Swing <i class="em em-golf"></i> </li>
                         </ul>
-                        <p class='bio'> You can reach me for an inquiry via email at<a class='contactEmoji' href="mailto:paul.s.kwon24@gmail.com"><strong>paul.s.kwon24@gmail.com</strong></a>or more information about myself: 
+                        <p class='bioText'> You can reach me for an inquiry via email at<a class='contactEmoji' href="mailto:paul.s.kwon24@gmail.com"><strong>paul.s.kwon24@gmail.com</strong></a>or more information about myself: 
                             <div class='moreInfo'>
                                 <a class='contactEmoji' href="https://www.linkedin.com/in/pskwon" target='_blank'><strong>LinkedIn</strong></a> <a class='contactEmoji' href="https://github.com/lilpkstud" target='_blank'><strong>Github</strong></a> <a class='contactEmoji' href="/views/resume.php" target='_blank'><strong>Resume</strong></a>
                             </div>
                         </p>
                     </div>
+                    
                 </div>
             </section>
         </div>
